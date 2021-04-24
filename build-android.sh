209914c42c9e6fabd2959415aa07eb390aa0b9c5
champclarinet <champbassist159@gmail.com>
2021-04-24 23:51:24 +0700

    init

---
+++ build-android.sh
@@ -0,0 +1,13 @@
+mkdir android/app/src/main/assets
+
+npx react-native bundle \
+    --platform android \
+    --dev false \
+    --entry-file index.js \
+    --bundle-output android/app/src/main/assets/index.android.bundle
+
+cd android
+
+./gradlew assembleRelease
+
+cd ..
